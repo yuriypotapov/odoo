5036b5c3758d0e8f1a32a5f799759c7e0de60384
vwvolodya <vwvolodya@gmail.com>
2015-04-29 16:06:19 +0300

    added view for auction

--- addons/website_sale/controllers/auction_controllers.py
+++ addons/website_sale/controllers/auction_controllers.py
@@ -7,7 +7,8 @@ from openerp.http import request
 class AuctionController(http.Controller):
     @http.route(['/auction/place_bid'], type='http', auth='user')
     def register_new_bid(self):
-        pass
+        cr, uid, context, pool = request.cr, request.uid, request.context, request.env
+        
 
     def check_bid(self):
         pass
